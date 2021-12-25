e9baee43c4095bfd0c099137864b01b6b2feec53
Diego Linares <diegojeanluck@hotmail.com>
2021-12-25 12:43:16 -0500

    The page is ready for now

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hollow Knight: Fan Page
+This page is my project of level to for "Holberton School".
+
+## I use:
+- HTML
+- CSS
+- JS
---
+++ js/behavior.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  for (let i = 0; i < 3; i++) {
+    let photo = document.getElementById(`${i.toString()}`);
+    photo.addEventListener("click", () => {
+      let classes = photo.className.split(" ");
+      classes.includes("small")
+        ? (photo.className = "photo")
+        : (photo.className = "photo small");
+    });
+  }
+});
