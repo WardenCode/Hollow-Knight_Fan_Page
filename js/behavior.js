dfb11769948d03a9fa1793024778a2d4f60c76a2
WardenCode <diegojeanluck@hotmail.com>
2022-04-16 11:40:31 -0500

    Using functions

--- js/behavior.js
+++ js/behavior.js
@@ -10,16 +10,33 @@
 //   }
 // });
 
-document.addEventListener("DOMContentLoaded", () => {
-  let photos = document.querySelectorAll(".photo");
-
-  for (let photo of photos) {
+function validateClass(photo) {
     photo.addEventListener("click", () => {
       let classes = photo.className.split(" ");
       classes.includes("small")
         ? (photo.className = "photo")
         : (photo.className = "photo small");
     }, false);
-  };
-  
+}
+
+function addTheEvents(photos) {
+  photos.forEach(photo => validateClass(photo));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  let photos = document.querySelectorAll(".photo");
+  addTheEvents(photos);
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   let photos = document.querySelectorAll(".photo");
+
+//   for (let photo of photos) {
+//     photo.addEventListener("click", () => {
+//       let classes = photo.className.split(" ");
+//       classes.includes("small")
+//         ? (photo.className = "photo")
+//         : (photo.className = "photo small");
+//     }, false);
+//   };  
+// });
